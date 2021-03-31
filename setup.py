946c55079abde4a0565a20ee679e2a8f1a8bcb2f
Marco F <m.faverof@gmail.com>
2021-03-31 15:18:33 +0200

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ds-easyconf',
-    version='1.1.0',
+    version='1.1.1',
     description='Easy dsconf manager',
     long_description=long_description,
     long_description_content_type="text/markdown",
